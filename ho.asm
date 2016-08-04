124987d562d53b912b1ce104063f806d71b9e7ed
sergiogb325 <checho9425@gmail.com>
2016-08-04 11:04:19 -0600

    Update ho.asm

--- ho.asm
+++ ho.asm
@@ -4,6 +4,8 @@
 section .data 
 	linea_uno: db 'H',0xa
 	l1_tamano: equ $-linea_uno
+	l2: db 'ola',0xa
+	l: equ $-l2
 	
 
 ;----------------------------------------------------
@@ -19,6 +21,12 @@ _start:
 	mov rsi,linea_uno-1
 
 	mov rdx,l1_tamano
+	syscall
+	
+	mov rax,1
+	mov rdi,1,
+	mov rsi,l2
+	mov rdx,l
 	syscall
 
 
